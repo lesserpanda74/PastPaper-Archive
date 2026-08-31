fdf561e0e7cbb1bfb7bdc9345a8e94a661860f06
lesserpanda74 <lesserpanda74@gmail.com>
2026-08-31 16:43:49 +0900

    Create examData.js

---
+++ js/data/examData.js
@@ -0,0 +1,106 @@
+export const examData = {
+
+    "1": {
+
+        "수학": {
+
+            "1단원": [
+
+                {
+                    type: "multiple",
+
+                    question: "5 + 3의 값은?",
+
+                    choices: [
+                        "6",
+                        "7",
+                        "8",
+                        "9"
+                    ],
+
+                    answer: 2,
+
+                    explanation:
+                        "5 + 3 = 8입니다."
+                },
+
+                {
+                    type: "multiple",
+
+                    question: "12 - 7의 값은?",
+
+                    choices: [
+                        "3",
+                        "4",
+                        "5",
+                        "6"
+                    ],
+
+                    answer: 2,
+
+                    explanation:
+                        "12 - 7 = 5입니다."
+                },
+
+                {
+                    type: "multiple",
+
+                    question: "4 × 6의 값은?",
+
+                    choices: [
+                        "20",
+                        "24",
+                        "28",
+                        "30"
+                    ],
+
+                    answer: 1,
+
+                    explanation:
+                        "4 × 6 = 24입니다."
+                },
+
+                {
+                    type: "multiple",
+
+                    question: "36 ÷ 6의 값은?",
+
+                    choices: [
+                        "4",
+                        "5",
+                        "6",
+                        "7"
+                    ],
+
+                    answer: 2,
+
+                    explanation:
+                        "36 ÷ 6 = 6입니다."
+                },
+
+                {
+                    type: "multiple",
+
+                    question:
+                        "x + 5 = 12일 때 x의 값은?",
+
+                    choices: [
+                        "5",
+                        "6",
+                        "7",
+                        "8"
+                    ],
+
+                    answer: 2,
+
+                    explanation:
+                        "12 - 5 = 7이므로 x = 7입니다."
+                }
+
+            ]
+
+        }
+
+    }
+
+};
